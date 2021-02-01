0a50fbcae2fe6424aa56067b64fb12b5cab0d627
Gregory <gregory.smith@talk21.com>
2021-02-01 20:09:46 +0000

    Remove acronym definitions from output

--- pandoc-acronym-filter.lua
+++ pandoc-acronym-filter.lua
@@ -2,22 +2,27 @@
 function Blocks(blocks)
     -- Get all the acronym definitions
     definitions = {}
+    filtered_blocks = {}
     for i, block in pairs(blocks) do
         if (block.tag == "Para") then
             local content = pandoc.utils.stringify(block.content)
             local acronym, description = string.match(content, '%*%[(.*)%]: (.*)')
             if (acronym) then
                 definitions[acronym] = description
+                -- Remove acronym definitions
+            else
+                table.insert(filtered_blocks, block)
             end
+        else
+            table.insert(filtered_blocks, block)
         end
     end
     -- Replace all the acronyms
     output = {}
-    for i, block in pairs(blocks) do
+    for i, block in pairs(filtered_blocks) do
         if (block.tag == "Para") then
             output[i] = pandoc.walk_block(block, {
                 Str = function(el)
-                    -- TODO: Remove acronym definition
                     -- Replace acronym with abbreviation code
                     -- TODO: Also need to check for plurals
                     for k, v in pairs(definitions) do
