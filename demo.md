511ef5e0634a7429b8d969f38d84f0debd6404d7
Gregory <gregory.smith@talk21.com>
2021-02-02 02:16:05 +0000

    A broken implementation for latex

--- demo.md
+++ demo.md
@@ -1,4 +1,10 @@
-# Demo
+---
+title: Demonstration of `pandoc-acronym-filter`
+header-includes: |
+    \usepackage[printonlyused,nohyperlinks]{acronym}
+---
+
+# Demonstration
 
 The HTML specification is maintained by the W3C. The use of abbreviations means that any use of the abbreviation, such as HTML or W3C is linked to the abbreviation.
 
@@ -12,12 +18,18 @@ Websites **use HTTP to communicate** their contents with browsers.
 
 This is a sentence that includes *some* text formatting which should not be affected by the acronym definition.
 
+# Plurals
+
 I have a lot of old CDs.
 
 *[CD]: Compact Disk
+
+# Unmodified
 
 ```
 Do not expect acronyms in code blocks, like this one HTML, to be modified.
 ```
 
 > And acronyms in quotations, like this one W3C, should also not be modified.
+
+# Acronyms
--- pandoc-acronym-filter.lua
+++ pandoc-acronym-filter.lua
@@ -1,4 +1,5 @@
 
+-- TODO: Add acronym package
 function Blocks(blocks)
     -- Get all the acronym definitions
     definitions = {}
@@ -30,9 +31,16 @@ function Blocks(blocks)
                         -- Match plural key (followed by s)
                         local match = string.match(el.text, '^' .. k .. '[s]?%p*$')
                         if match then
-                            local acronym = "<abbr title=\"" .. v .. "\">" .. k .. "</abbr>"
-                            local text = string.gsub(el.text, k, acronym, 1)
-                            return pandoc.RawInline("html", text)
+                            if FORMAT == 'html' then
+                                local acronym = "<abbr title=\"" .. v .. "\">" .. k .. "</abbr>"
+                                local text = string.gsub(el.text, k, acronym, 1)
+                                return pandoc.RawInline("html", text)
+                            elseif FORMAT == 'latex' then
+                                local acronym = "\\ac{" .. k .. "}"
+                                local text = string.gsub(el.text, k, acronym, 1)
+                                return pandoc.RawInline("latex", text)
+                            end
+                            return el.text
                         end
                     end
                     return el
@@ -42,6 +50,18 @@ function Blocks(blocks)
             output[i] = block
         end
     end
-    -- TODO: Add acronym list somewhere. At end? Configure heading?
+    -- Add list of acronyms
+    -- TODO: Sort acronyms alphabetically https://stackoverflow.com/questions/26160327/sorting-a-lua-table-by-key
+    -- TODO: Fix so that only done at end of doc not at end of list of blocks
+    -- TODO: Add acronym to "acronym" header/config header or at end of doc
+    if FORMAT == 'latex' then
+        table.insert(output, pandoc.RawBlock("latex", "\\begin{acronym}"))
+        for k, v in pairs(definitions) do
+            local acronym = "\\acro{" .. k .. "}{" .. v .. "}"
+            local block = pandoc.RawBlock("latex", acronym)
+            table.insert(output, block)
+        end
+        table.insert(output, pandoc.RawBlock("latex", "\\end{acronym}"))
+    end
     return output
 end
